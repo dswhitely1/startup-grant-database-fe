26331ab35fa87f585121175ea0073bf74bed4dc6
spenclark <spenclark@hotmail.com>
2020-01-28 15:19:39 -0800

    made more readable

--- grantly/src/components/userSettings/SettingsPage.js
+++ grantly/src/components/userSettings/SettingsPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { connect } from "react-redux";
 import { useAuth0 } from "../../react-auth0-wrapper";
+import { Link }from 'react-router-dom'
+
+// material imports
+import {Grid} from "@material-ui/core";
 
 import SettingsForm from './SettingsForm'
 import SettingsCard from './SettingsCard'
@@ -8,12 +12,27 @@ const SettingsPage = props => {
     
     return (
         <>
-        // if props .length = 0 do not render!
+        
+        <Grid item xs={6}>
+            <Link>
+                Change Password
+            </Link>
+        </Grid>
+        <Grid item xs={6}>
+            <Link>
+                Change Email
+            </Link>
+        </Grid>
+{/* 
+        // if props .length = 0 do not render! */}
         <SettingsCard />
+        {/* // once we have functionaliy:
+            a) make form and card have isEditing state for better UI */}
+
         <br></br>
         <SettingsForm />
 
-
+        
         </>
     )
 }
